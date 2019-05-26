4df8bef575d7f9adf62cef61f02354a100a4de73
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-05-26 13:42:08 -0700

    added race and length dropdowns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { GenderComponent } from './gender/gender.component';
+import { RaceComponent } from './race/race.component';
+import { LengthComponent } from './length/length.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GenderComponent
+    GenderComponent,
+    RaceComponent,
+    LengthComponent
   ],
   imports: [
     BrowserModule,
